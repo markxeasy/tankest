c041a99489b69a344d71922e120f0bdce79c020e
Marc <marc.moser@dracodex.com>
2019-09-29 22:45:18 +0200

    created hall of fame page and inserted first records

---
+++ src/components/RecordCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card
+    :color="record.color"
+    dark
+  >
+    <v-list-item three-line>
+      <v-list-item-content class="align-self-start">
+        <v-list-item-title
+          class="headline mb-2"
+          v-text="record.title"
+        ></v-list-item-title>
+
+        <v-list-item-subtitle v-text="record.holder"></v-list-item-subtitle>
+        <v-list-item-subtitle v-text="record.value"></v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar
+        size="125"
+        tile
+      >
+        <v-icon>{{record.icon}}</v-icon>
+      </v-list-item-avatar>
+    </v-list-item>
+  </v-card>
+</template>
+<script>
+export default {
+  name: 'RecordCard',
+  props: ['record'],
+  methods: {},
+  created () {},
+}
+</script>
